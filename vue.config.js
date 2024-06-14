2a17d0f426a4dc86ae726240be35bd0af5b526ae
scdow <920301714@qq.com>
2024-06-15 03:02:32 +0800

    stop display error resizeObserver

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,28 @@
-const {defineConfig} = require('@Vue/Cli-service')
+const {defineConfig} = require('@Vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies:true,
   devServer:{
-    port:7070
+    port:7070,
+    
+    //Stop error resizeObserver
+    client: {
+      overlay: {
+        runtimeErrors: (error) => {
+          const ignoreErrors = [
+            "ResizeObserver loop limit exceeded",
+            "ResizeObserver loop completed with undelivered notifications.",
+            "[eslint]",
+          ];
+          if (ignoreErrors.includes(error.message)) {
+            return false;
+          }
+          return true;
+        },
+        warnings: false,  // 不显示警告
+      },
+    },
+
   }
 })
+
+
